6340a1d6c9876ca818a943fd3796d23511ac4dae
Gilbert Luis <gilbertluis97@gmail.com>
2024-10-14 15:48:22 -0400

    adding footer

--- pages/index.vue
+++ pages/index.vue
@@ -5,21 +5,35 @@
       <ModuleSelection :modules="quizData.modules" @select="startQuiz" />
     </template>
     <template v-else>
-      <h2 class="text-2xl font-bold mb-4">Pregunta {{ currentQuestionIndex + 1 }} de {{ questions.length }}</h2>
-      <QuizQuestions
-        :question="currentQuestion"
-        @answer="handleAnswer"
-      />
+      <h2 class="text-2xl font-bold mb-4">
+        Pregunta {{ currentQuestionIndex + 1 }} de {{ questions.length }}
+      </h2>
+      <QuizQuestions :question="currentQuestion" @answer="handleAnswer" />
       <p class="mt-4">Puntuación actual: {{ score }}</p>
-      <UButton v-if="quizCompleted" @click="finishQuiz" class="mt-4">Finalizar Quiz</UButton>
+      <UButton v-if="quizCompleted" @click="finishQuiz" class="mt-4"
+        >Finalizar Quiz</UButton
+      >
     </template>
   </div>
+  <footer>
+    <p class="flex items-center justify-center">Hecho con ❤ por Gilbert</p>
+  </footer>
 </template>
 
 <script setup lang="ts">
-import { useQuiz } from '~/composable/usequiz';
-import quizData from '@/assets/questions.json'
-import QuizQuestions from '~/components/quizQuestions.vue';
+import { useQuiz } from "~/composable/usequiz";
+import quizData from "@/assets/questions.json";
+import QuizQuestions from "~/components/quizQuestions.vue";
 
- const {selectedModule, startQuiz, questions, currentQuestionIndex, score, currentQuestion, handleAnswer, quizCompleted, finishQuiz}= useQuiz()
+const {
+  selectedModule,
+  startQuiz,
+  questions,
+  currentQuestionIndex,
+  score,
+  currentQuestion,
+  handleAnswer,
+  quizCompleted,
+  finishQuiz,
+} = useQuiz();
 </script>
