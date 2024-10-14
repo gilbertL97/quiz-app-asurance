4c143def2d38942c69d3baaa6d5582a00539a360
Gilbert Luis <gilbertluis97@gmail.com>
2024-10-14 16:08:48 -0400

    make grid the module selection

--- components/moduleSelection.vue
+++ components/moduleSelection.vue
@@ -2,30 +2,34 @@
 <template>
   <div>
     <h2 class="text-2xl font-bold mb-4">Selecciona un módulo</h2>
-    <UCard v-for="module in modules" :key="module.id" class="mb-4">
-      <template #header>
-        <h3 class="text-lg font-semibold">{{ module.name }}</h3>
-      </template>
-      <p>Este módulo contiene {{ module.questions.length }} preguntas.</p>
-      <template #footer>
-        <UButton @click="selectModule(module.id)">Comenzar este módulo</UButton>
-      </template>
-    </UCard>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <UCard v-for="module in modules" :key="module.id" class="mb-4">
+        <template #header>
+          <h3 class="text-lg font-semibold">{{ module.name }}</h3>
+        </template>
+        <p>Este módulo contiene {{ module.questions.length }} preguntas.</p>
+        <template #footer>
+          <UButton @click="selectModule(module.id)"
+            >Comenzar este módulo</UButton
+          >
+        </template>
+      </UCard>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { QuizModuleTypes } from '@/types/quiz.types'
+import type { QuizModuleTypes } from "@/types/quiz.types";
 
 const props = defineProps<{
-  modules: QuizModuleTypes[]
-}>()
+  modules: QuizModuleTypes[];
+}>();
 
 const emit = defineEmits<{
-  (e: 'select', moduleId: string): void
-}>()
+  (e: "select", moduleId: string): void;
+}>();
 
 const selectModule = (moduleId: string) => {
-  emit('select', moduleId)
-}
+  emit("select", moduleId);
+};
 </script>
