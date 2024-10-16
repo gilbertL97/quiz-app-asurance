db79f4de32e5f63d65cad13bb32f38c461b732e8
Gilbert Luis <gilbertluis97@gmail.com>
2024-10-16 00:19:16 -0400

    changin temo for modulo

--- components/moduleSelection.vue
+++ components/moduleSelection.vue
@@ -1,16 +1,16 @@
 // components/ModuleSelection.vue
 <template>
   <div>
-    <h2 class="text-2xl font-bold mb-4">Selecciona un módulo</h2>
+    <h2 class="text-2xl font-bold mb-4">Selecciona un tema</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       <UCard v-for="module in modules" :key="module.id" class="mb-4">
         <template #header>
           <h3 class="text-lg font-semibold">{{ module.name }}</h3>
         </template>
-        <p>Este módulo contiene {{ module.questions.length }} preguntas.</p>
+        <p>Este tema contiene {{ module.questions.length }} preguntas.</p>
         <template #footer>
           <UButton @click="selectModule(module.id)"
-            >Comenzar este módulo</UButton
+            >Comenzar este tema</UButton
           >
         </template>
       </UCard>
