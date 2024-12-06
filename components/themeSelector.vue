a7ef05cf6cffc4d6792dc08b1699ade881fe5631
Gilbert Luis <gilbertluis97@gmail.com>
2024-12-06 13:04:28 -0500

    change name

---
+++ components/themeSelector.vue
@@ -0,0 +1,33 @@
+// components/ModuleSelection.vue
+<template>
+  <div>
+    <h2 class="text-2xl font-bold mb-4">Selecciona un tema</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <UCard v-for="module in modules" :key="module.id" class="mb-4">
+        <template #header>
+          <h3 class="text-lg font-semibold">{{ module.name }}</h3>
+        </template>
+        <p>Este tema contiene {{ module.questions.length }} preguntas.</p>
+        <template #footer>
+          <UButton @click="selectModule(module.id)">Comenzar este tema</UButton>
+        </template>
+      </UCard>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { QuizModuleTypes } from "@/types/quiz.types";
+
+const props = defineProps<{
+  modules: QuizModuleTypes[];
+}>();
+
+const emit = defineEmits<{
+  (e: "select", moduleId: string): void;
+}>();
+
+const selectModule = (moduleId: string) => {
+  emit("select", moduleId);
+};
+</script>
--- pages/theme.vue
+++ pages/theme.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="container mx-auto px-4 py-8">
     <template v-if="!selectedModule">
-      <ThemeSelector :modules="quizData.modules" @select="startQuiz" />
+      <ModuleSelection :modules="quizData.modules" @select="startQuiz" />
     </template>
     <template v-else>
       <h2 class="text-2xl font-bold mb-4">
@@ -10,7 +10,9 @@
       </h2>
       <QuizQuestions :question="currentQuestion" @answer="handleAnswer" />
       <p class="mt-4">Puntuación actual: {{ score }}</p>
-      <UButton v-if="quizCompleted" @click="finishQuiz" class="mt-4">Finalizar Quiz</UButton>
+      <UButton v-if="quizCompleted" @click="finishQuiz" class="mt-4"
+        >Finalizar Quiz</UButton
+      >
     </template>
   </div>
 </template>
@@ -19,7 +21,6 @@
 import { useQuiz } from "~/composable/usequiz";
 import quizData from "@/assets/questions.json";
 import QuizQuestions from "~/components/quizQuestions.vue";
-import ThemeSelector from "~/components/themeSelector.vue";
 
 const {
   selectedModule,
